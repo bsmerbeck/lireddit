11f7acc20260ca7f8363f85a06e5f53833376e4f
Brenden Smerbeck <bsmerbeck@acumentrics.com>
2021-08-16 21:52:15 -0400

    added some login and register functionality. added some validation as well

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,10 @@ import { __prod__ } from "./constants";
 import microConfig from './mikro-orm.config';
 import express from 'express';
 import {ApolloServer} from "apollo-server-express";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
 import {buildSchema} from 'type-graphql'
 import { HelloResolver } from "./resolvers/hello";
-import { PostResolver } from "./resolvers/post";
-
-import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
+import { UserResolver } from "./resolvers/user";
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
@@ -18,7 +17,7 @@ const main = async () => {
 
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
-            resolvers: [HelloResolver, PostResolver],
+            resolvers: [HelloResolver, UserResolver],
             validate: false,
             
         }),
